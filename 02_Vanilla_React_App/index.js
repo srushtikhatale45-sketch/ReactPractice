2da14aed5926907bfc89cfd86bbee404421b6f7d
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-02-12 15:40:17 +0530

    Introduction

---
+++ 02_Vanilla_React_App/index.js
@@ -0,0 +1,8 @@
+console.log("Script alert");
+
+// !### case1
+const rootElement = document.getElementById("root");
+// We have linked React library by CDN:
+// Using React.createElement Method of it:
+// createElement takes (type,props,children)
+const element= React.createElement("h1",null,"Hello World");
--- 5_all_about_state/src/main.jsx
+++ 5_all_about_state/src/main.jsx
@@ -1,8 +1,9 @@
 // import { StrictMode } from 'react'
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './Component/Users/user.css';
 import App from './App.jsx';
+import './index.css'
 import Example from './Example.jsx';
 
 
